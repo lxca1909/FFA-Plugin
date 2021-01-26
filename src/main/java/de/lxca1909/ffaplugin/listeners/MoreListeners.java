55e41b0c28781895c494acc4f300d0597ec0dbff
lucaott <luottmkl@gmail.com>
2021-01-27 00:03:23 +0100

    Edit MoreListeners Death Event

--- src/main/java/de/lxca1909/ffaplugin/listeners/MoreListeners.java
+++ src/main/java/de/lxca1909/ffaplugin/listeners/MoreListeners.java
@@ -83,7 +83,6 @@ public class MoreListeners implements Listener {
                 e.setCancelled(false);
             }else
                 e.setCancelled(true);
-
         }catch (Exception e1){
 
         }
